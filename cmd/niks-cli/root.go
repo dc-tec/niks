4b51ffc6d713d0e5103961482efaca43d3c0d6d1
Roel de Cort <roel@decort.tech>
2024-04-26 13:56:16 +0200

    create base

---
+++ cmd/niks-cli/root.go
@@ -0,0 +1,27 @@
+package nikscli
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "niks-cli",
+	Short: "A small utility wrapper to simplify working with NixOS and the Nix package manager.",
+	Long:  `A small utility wrapper to simplify working with NixOS and the Nix package manager.`,
+	Run: func(cmd *cobra.Command, args []string) {
+
+	},
+}
+
+func Execute() {
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "Yo, waddup?. We encounterd an error: %s\n", err)
+		os.Exit(1)
+	}
+
+}
+
