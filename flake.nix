c6c81695c25e5c30d572131bff59ed6afa7f26c2
Roel de Cort <63876068+dc-tec@users.noreply.github.com>
2024-04-29 00:20:47 +0200

    Development (#2)
    * create base
    
    * add logic
    
    * create commands
    
    * add run commands
    
    * refactor command call
    
    * append the configuration name
    
    * add option to define a custom path
    
    * add output
    
    * remove dryrun and add default path
    
    * add auto elevation and streaming output
    
    * debugging
    
    * add get generations command
    
    * test garbage collection
    
    * init flake
    
    * update README
    
    * update generate output
    
    * formatting
    
    * change package name
    
    * add vendors
    
    * update flake

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "A basic gomod2nix flake";
+  description = "Niks-CLI a small Nix(OS) utility wrapper";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
